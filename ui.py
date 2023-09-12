88a12af14d9d9a25a8139df423753163c48dd89c
PythonZem <pythonzem@gmail.com>
2023-09-12 09:27:14 +0300

    add interface, all buttons, text question, and score

--- ui.py
+++ ui.py
@@ -8,5 +8,29 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, background=THEME_COLOR)
+
+        self.score = Label(text="Score: 0", background=THEME_COLOR, fg="white")
+        self.score.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, background="White")
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     text="Some question text",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        TRUE_IMG = PhotoImage(file="images/true.png")
+        self.buttons_true = Button(image=TRUE_IMG, highlightthickness=0)
+        self.buttons_true.grid(column=0, row=2)
+
+        FALSE_IMG = PhotoImage(file="images/false.png")
+        self.buttons_false = Button(image=FALSE_IMG, highlightthickness=0)
+        self.buttons_false.grid(column=1, row=2)
+
 
         self.window.mainloop()
+
+    # def show_question(self):
+
