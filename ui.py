cec56fb45135b632672466e75dcb3f582aff6028
PythonZem <pythonzem@gmail.com>
2023-09-13 09:36:59 +0300

    Conect UI with quiz_brain for showing questiun text

--- ui.py
+++ ui.py
@@ -1,21 +1,23 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
-
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, background=THEME_COLOR)
 
-        self.score = Label(text="Score: 0", background=THEME_COLOR, fg="white")
+        self.score = Label(text=f"Score: 0", background=THEME_COLOR, fg="white")
         self.score.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250, background="White")
         self.question_text = self.canvas.create_text(150,
                                                      125,
+                                                     width=280,
                                                      text="Some question text",
                                                      fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
@@ -28,9 +30,10 @@ class QuizInterface:
         FALSE_IMG = PhotoImage(file="images/false.png")
         self.buttons_false = Button(image=FALSE_IMG, highlightthickness=0)
         self.buttons_false.grid(column=1, row=2)
-
+        self.get_next_question()
 
         self.window.mainloop()
 
-    # def show_question(self):
-
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
